b70abd553b4f007eaa6adecd49202f23c780164e
Konomi <konomikitten@gmail.com>
2017-03-15 08:34:55 +1100

    Create google-homepage-tweaks.user.js
    Initial commit

---
+++ google-homepage-tweaks.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name        Google Homepage Tweaks
+// @author      KonomiKitten
+// @namespace   googlehomepagetweaks
+// @description Disable "Come here often? Make Google your homepage"
+// @include     http://*google.com*
+// @include     https://*google.com*
+// @version     1
+// @updateURL   https://github.com/KonomiKitten/greasemonkey-scripts/raw/master/google-homepage-tweaks.user.js
+// @grant       GM_addStyle
+// ==/UserScript==
+
+GM_addStyle (
+  /* Matched from:
+  
+  <div class="gb_Sc gb_Tc gb_Lc gb_g" aria-label="promo" role="region" data-ved="0EPNDCBU" style="behavior:url(#default#userData)">
+    <div class="gb_ha">
+  */
+  "div[aria-label=promo] > div { display: none !important; }"
+);

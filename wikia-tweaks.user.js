a5286de7cd0add2f488bbaeffb5b54ff1a26d600
Konomi <konomikitten@gmail.com>
2017-06-03 23:30:01 +1000

    Update wikia-tweaks.user.js

--- wikia-tweaks.user.js
+++ wikia-tweaks.user.js
@@ -5,16 +5,19 @@
 // @description Wikia Tweaks
 // @include     http://*wikia.com*
 // @include     https://*wikia.com*
-// @version     3.0.2
+// @version     3.0.3
 // @updateURL   https://github.com/KonomiKitten/greasemonkey-scripts/raw/master/wikia-tweaks.user.js
 // @grant       GM_addStyle
 // @run-at      document-start
 // ==/UserScript==
 
 GM_addStyle (
-  /* Hide Watch LIVE now dialog */
-  'div[id="globalNavigation"][class="wds-global-navigation"] { display: none !important; }'+
+  /* Hide Wikia main banner (Top bar) */
+  'div[id="globalNavigation"][class*="wds-global-navigation"] { display: none !important; }'+
+  
+  /* Hide Notifcation dialogs */
+  'div[id="wikiNotifications"][class*="wikiNotifications"] { display: none !important; }'+
 
-  /* Hide Explore the Beautiful world of Wikia */
+  /* Hide Explore the Beautiful world of Wikia (Bottom bar)*/
   'div[id="WikiaBarWrapper"][class="WikiaBarWrapper"] { display: none !important; }'
 );

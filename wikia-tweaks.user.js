7b1ca4791925dec7eaad346967d5a18109e3b63b
Konomi <konomikitten@gmail.com>
2017-06-03 23:35:22 +1000

    Update wikia-tweaks.user.js

--- wikia-tweaks.user.js
+++ wikia-tweaks.user.js
@@ -5,7 +5,7 @@
 // @description Wikia Tweaks
 // @include     http://*wikia.com*
 // @include     https://*wikia.com*
-// @version     3.0.3
+// @version     3.0.4
 // @updateURL   https://github.com/KonomiKitten/greasemonkey-scripts/raw/master/wikia-tweaks.user.js
 // @grant       GM_addStyle
 // @run-at      document-start
@@ -16,7 +16,10 @@ GM_addStyle (
   'div[id="globalNavigation"][class*="wds-global-navigation"] { display: none !important; }'+
   
   /* Hide Notifcation dialogs */
-  'div[id="wikiNotifications"][class*="wikiNotifications"] { display: none !important; }'+
+  'ul[id="wikiNotifications"][class*="wikiNotifications"] { display: none !important; }'+
+  
+  /* Hide Trending Fandom Articles */
+  'div[id="WikiRailWrapper"][class*="WikiRail"] { display: none !important; }'+
 
   /* Hide Explore the Beautiful world of Wikia (Bottom bar)*/
   'div[id="WikiaBarWrapper"][class="WikiaBarWrapper"] { display: none !important; }'

e573811af7442098d300d6b8551fb1d739610999
Konomi <konomikitten@gmail.com>
2017-10-07 06:25:14 +1100

    Update youtube-disable-up-next.user.js
    Making this was torture

--- youtube-disable-up-next.user.js
+++ youtube-disable-up-next.user.js
@@ -5,27 +5,36 @@
 // @description	Disable "Up Next" otherwise known as AutoPlay
 // @include	http://www.youtube.com*
 // @include	https://www.youtube.com*
-// @version	0.0.1
+// @version	1.0.0
 // @updateURL	https://github.com/KonomiKitten/greasemonkey-scripts/raw/master/youtube-disable-up-next.user.js
 // @grant	none
 // @run-at	document-end
 // ==/UserScript==
 
-function f() {
-	try {
-		var node;
-		node = document.querySelector(
-			'paper-toggle-button[aria-label="Autoplay"]'
-		);
-		if (typeof node === undefined) {
-				return;
-		}
-		//node.setAttribute('aria-pressed', 'false');
-		//node.removeAttribute('checked');
-		//node.removeAttribute('active');
-		console.log('node', node);
-	}
-	catch (e) {}
+function delayedObserver(args) {
+	new window.MutationObserver(function() {
+		 var element = document.querySelector(args.selector);
+		 if (element) {
+			 this.disconnect();
+			 args.done(element);
+		 }
+	}).observe(document, {
+		'childList': true,
+		'subtree': true
+	});
 }
-f();
-document.body.addEventListener('DOMSubtreeModified', f, false);
+delayedObserver({
+	selector: 'div#head.style-scope.ytd-compact-autoplay-renderer',
+	done: function(element) {
+		element.style.setProperty("display", "none", "important");
+	}
+});
+delayedObserver({
+	selector: 'paper-toggle-button#toggle'
+		+'.style-scope.ytd-compact-autoplay-renderer',
+	done: function(element) {
+		if (element.checked) {
+			element.click();
+		}
+	}
+});

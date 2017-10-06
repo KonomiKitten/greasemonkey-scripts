7bae3d89f043d4f490f40727e21c7113496f7739
Konomi <konomikitten@gmail.com>
2017-10-06 22:18:19 +1100

    Create youtube-disable-up-next.user.js

---
+++ youtube-disable-up-next.user.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name	YouTube Disable Up Next
+// @author	KonomiKitten
+// @namespace	youtubedisableupnext
+// @description	Disable "Up Next" otherwise known as AutoPlay
+// @include	http://www.youtube.com*
+// @include	https://www.youtube.com*
+// @version	1.0.0
+// @updateURL	https://github.com/KonomiKitten/greasemonkey-scripts/raw/master/youtube-disable-up-next.user.js
+// @grant	none
+// @run-at	document-end
+// ==/UserScript==
+
+function f() {
+	try {
+		var node;
+		node = document.querySelector(
+			'paper-toggle-button[aria-label="Autoplay"]'
+		);
+		node.setAttribute('aria-pressed', 'false');
+	}
+	catch (e) {}
+}
+f();
+document.body.addEventListener('DOMSubtreeModified', f, false);

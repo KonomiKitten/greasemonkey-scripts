0b4e96d9f41e85492d13fae9a5957f06c434a21e
Konomi <konomikitten@gmail.com>
2017-12-17 04:42:18 +1100

    Update twitter-tweaks.user.js
    Update script for Greasemonkey 4.x

--- twitter-tweaks.user.js
+++ twitter-tweaks.user.js
@@ -1,31 +1,40 @@
 // ==UserScript==
-// @name        Twitter Tweaks
-// @author      KonomiKitten
-// @namespace   twittetweaks
-// @description Twitter Tweaks
-// @include     http://twitter.com*
-// @include     https://twitter.com*
-// @version     3.0.3
-// @updateURL   https://github.com/KonomiKitten/greasemonkey-scripts/raw/master/twitter-tweaks.user.js
-// @grant       GM_addStyle
-// @run-at      document-start
+// @name	Twitter Tweaks
+// @author	KonomiKitten
+// @namespace	twittetweaks
+// @description	Twitter Tweaks
+// @include	http://twitter.com*
+// @include	https://twitter.com*
+// @version	3.0.4
+// @updateURL	https://github.com/KonomiKitten/greasemonkey-scripts/raw/master/twitter-tweaks.user.js
+// @run-at	document-start
 // ==/UserScript==
 
-GM_addStyle (
-  /* Hide Moments button */
-  'li[class*="moments js-moments-tab"] { display: none !important; }'+
-  
-  /* Hide Who to follow */
-  'div[class*="wtf-module js-wtf-module"], div[class*="WhoToFollow"], '+
-  'li[data-component-context*="suggest_who_to_follow"] { display: none !important; }'+
-  
-  /* Hide Advertise with Twitter */
-  'div[class*="flex-module Footer-adsModule"] { display: none !important; }'+
-  
-  /* Hide While you were away... */
-  'li[class*="js-stream-item"][class*="has-recap"] { display: none !important; }'+
-  
-  /* Hide Live video */
-  'div[class*="VideoGuide"][class*="roaming-module"] { display: none !important; }'+
-  'div[class*="LiveVideoHomePageModuleContainer"][class*="roaming-module"] { display: none !important; }'
+function add_global_style(css) {
+	var head, style;
+	head = document.getElementsByTagName('head')[0];
+	if (!head) { return; }
+	style = document.createElement('style');
+	style.type = 'text/css';
+	style.innerHTML = css;
+	head.appendChild(style);
+}
+
+add_global_style(
+	/* Hide Moments button */
+	'li[class*="moments js-moments-tab"] { display: none !important; }'+
+
+	/* Hide Who to follow */
+	'div[class*="wtf-module js-wtf-module"], div[class*="WhoToFollow"], '+
+	'li[data-component-context*="suggest_who_to_follow"] { display: none !important; }'+
+
+	/* Hide Advertise with Twitter */
+	'div[class*="flex-module Footer-adsModule"] { display: none !important; }'+
+
+	/* Hide While you were away... */
+	'li[class*="js-stream-item"][class*="has-recap"] { display: none !important; }'+
+
+	/* Hide Live video */
+	'div[class*="VideoGuide"][class*="roaming-module"] { display: none !important; }'+
+	'div[class*="LiveVideoHomePageModuleContainer"][class*="roaming-module"] { display: none !important; }'
 );

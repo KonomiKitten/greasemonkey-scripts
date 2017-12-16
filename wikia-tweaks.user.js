17389be253c1858c34880216fb1b99f271ef8b16
Konomi <konomikitten@gmail.com>
2017-12-17 04:56:34 +1100

    Update wikia-tweaks.user.js
    Update script for Greasemonkey 4.x

--- wikia-tweaks.user.js
+++ wikia-tweaks.user.js
@@ -1,29 +1,38 @@
 // ==UserScript==
-// @name        Wikia Tweaks
-// @author      KonomiKitten
-// @namespace   wikiatweaks
-// @description Wikia Tweaks
-// @include     http://*wikia.com*
-// @include     https://*wikia.com*
-// @version     3.0.6
-// @updateURL   https://github.com/KonomiKitten/greasemonkey-scripts/raw/master/wikia-tweaks.user.js
-// @grant       GM_addStyle
-// @run-at      document-start
+// @name	Wikia Tweaks
+// @author	KonomiKitten
+// @namespace	wikiatweaks
+// @description	Wikia Tweaks
+// @include	http://*wikia.com*
+// @include	https://*wikia.com*
+// @version	3.0.6
+// @updateURL	https://github.com/KonomiKitten/greasemonkey-scripts/raw/master/wikia-tweaks.user.js
+// @run-at	document-start
 // ==/UserScript==
 
-GM_addStyle (
-  /* Hide Wikia main banner (Top bar) */
-  'div[id="globalNavigation"][class*="wds-global-navigation"] { display: none !important; }'+
-  
-  /* Hide Notifcation dialogs */
-  'ul[id="WikiaNotifications"][class*="WikiaNotifications"] { display: none !important; }'+
-  
-  /* Hide Trending Fandom Articles */
-  'div[id="WikiaRailWrapper"][class*="WikiaRail"] { display: none !important; }'+
+function add_global_style(css) {
+	var head, style;
+	head = document.getElementsByTagName('head')[0];
+	if (!head) { return; }
+	style = document.createElement('style');
+	style.type = 'text/css';
+	style.innerHTML = css;
+	head.appendChild(style);
+}
 
-  /* Hide Fan Feed */
-  'div[id="recirculation-footer-index-container"] { display: none !important; }'+
+add_global_style(
+	/* Hide Wikia main banner (Top bar) */
+	'div[id="globalNavigation"][class*="wds-global-navigation"] { display: none !important; }'+
 
-  /* Hide Explore the Beautiful world of Wikia (Bottom bar)*/
-  'div[id="WikiaBarWrapper"][class="WikiaBarWrapper"] { display: none !important; }'
+	/* Hide Notifcation dialogs */
+	'ul[id="WikiaNotifications"][class*="WikiaNotifications"] { display: none !important; }'+
+
+	/* Hide Trending Fandom Articles */
+	'div[id="WikiaRailWrapper"][class*="WikiaRail"] { display: none !important; }'+
+
+	/* Hide Fan Feed */
+	'div[id="recirculation-footer-index-container"] { display: none !important; }'+
+
+	/* Hide Explore the Beautiful world of Wikia (Bottom bar)*/
+	'div[id="WikiaBarWrapper"][class="WikiaBarWrapper"] { display: none !important; }'
 );

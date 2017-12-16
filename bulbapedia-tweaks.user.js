6238d0344133fdf37d276d4c3c2a08c4b8ee03cd
Konomi <konomikitten@gmail.com>
2017-12-17 05:10:04 +1100

    Update bulbapedia-tweaks.user.js
    Update script for Greasemonkey 4.x

--- bulbapedia-tweaks.user.js
+++ bulbapedia-tweaks.user.js
@@ -1,20 +1,25 @@
 // ==UserScript==
-// @name        Bulbapedia Tweaks
-// @author      KonomiKitten
-// @namespace   bulbapediatweaks
-// @description Hides the notice banner
-// @include     http://bulbapedia.bulbagarden.net/*
-// @version     3
-// @updateURL   https://github.com/KonomiKitten/greasemonkey-scripts/raw/master/bulbapedia-tweaks.user.js
-// @grant       GM_addStyle
-// @run-at      document-start
+// @name	Bulbapedia Tweaks
+// @author	KonomiKitten
+// @namespace	bulbapediatweaks
+// @description	Hides the notice banner
+// @include 	http://bulbapedia.bulbagarden.net/*
+// @version	3.0.1
+// @updateUR	https://github.com/KonomiKitten/greasemonkey-scripts/raw/master/bulbapedia-tweaks.user.js
+// @run-at	document-start
 // ==/UserScript==
 
-GM_addStyle (
-  /* Matched from:
-  
-  <div id="siteNotice">
-  
-  */
+function add_global_style(css) {
+	var head, style;
+	head = document.getElementsByTagName('head')[0];
+	if (!head) { return; }
+	style = document.createElement('style');
+	style.type = 'text/css';
+	style.innerHTML = css;
+	head.appendChild(style);
+}
+
+add_global_style (
+  /* Disable Notice Banner */
   "div#siteNotice { display: none !important; }"
 );

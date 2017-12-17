b7886683c133ceffd593fac034a73e5410951876
Konomi <konomikitten@gmail.com>
2017-12-17 19:07:39 +1100

    Small change to test autoupdating
    Testing

--- google-homepage-tweaks.user.js
+++ google-homepage-tweaks.user.js
@@ -5,13 +5,11 @@
 // @description	Disable "Come here often? Make Google your homepage"
 // @include	http://www.google.com*
 // @include	https://www.google.com*
-// @version	3.0.2
+// @version	3.0.3
 // @updateURL	https://github.com/KonomiKitten/userscripts/raw/master/google-homepage-tweaks.user.js
 // @grant	none
 // @run-at	document-end
 // ==/UserScript==
 
-/* Hides "Come here often? Make Google your homepage"
-<div class="gb_fa gb_g" aria-hidden="false">
-	<a class="gb_2c gb_od" href="javascript:void(0)" title="No thanks">×</a> */
+/* Hides "Come here often? Make Google your homepage" */
 document.querySelector('a[title="No thanks"]').parentNode.style.setProperty("display", "none", "important");

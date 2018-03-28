723bcf624cd7441d28e4e9ab19a97f417aa42baa
Konomi <konomikitten@gmail.com>
2018-03-29 00:51:03 +1100

    Use window.MutationObserver

--- bulbapedia-tweaks.user.js
+++ bulbapedia-tweaks.user.js
@@ -4,22 +4,34 @@
 // @namespace	bulbapediatweaks
 // @description	Hides the notice banner
 // @include 	http://bulbapedia.bulbagarden.net/*
-// @version	3.0.2
+// @version	3.0.3
 // @updateURL	https://github.com/KonomiKitten/userscripts/raw/master/bulbapedia-tweaks.user.js
 // @run-at	document-start
 // ==/UserScript==
 
-function add_global_style(css) {
-	var head, style;
-	head = document.getElementsByTagName('head')[0];
-	if (!head) { return; }
-	style = document.createElement('style');
-	style.type = 'text/css';
-	style.innerHTML = css;
-	head.appendChild(style);
+function mutationObserverByTag(args) {
+	new window.MutationObserver(function() {
+		var element = document.getElementsByTagName(args.tag)[0];
+		if (element) {
+			 this.disconnect();
+			 args.done(element);
+		}
+	}).observe(document, {
+		'childList': true,
+		'subtree': true
+	});
 }
+mutationObserverByTag({
+	tag: 'head',
+	done: function(element) {
+		var css = /* Cascading Style Sheets */
+		/* Disable Notice Banner */
+		'div#siteNotice { display: none !important; }';
 
-add_global_style (
-  /* Disable Notice Banner */
-  "div#siteNotice { display: none !important; }"
-);
+		/* Insert the style into the page */
+		style = document.createElement('style');
+		style.type = 'text/css';
+		style.innerHTML = css;
+		element.appendChild(style);
+	}
+});

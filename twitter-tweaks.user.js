e10b9bd16ce7e83de1b34b72eff4a6813c482893
KonomiKitten <konomikitten@gmail.com>
2019-05-18 22:25:09 +1000

    Use GM_addStyle for Twitter Tweaks

--- twitter-tweaks.user.js
+++ twitter-tweaks.user.js
@@ -5,61 +5,40 @@
 // @description Twitter Tweaks
 // @include     http://twitter.com*
 // @include     https://twitter.com*
-// @version     3.1.6
+// @version     3.1.7
 // @updateURL   https://github.com/konomikitten/userscripts/raw/master/twitter-tweaks.user.js
 // @downloadURL https://github.com/konomikitten/userscripts/raw/master/twitter-tweaks.user.js
 // @homepageURL https://github.com/konomikitten/userscripts
 // @supportURL  https://github.com/konomikitten/userscripts/issues
 // @icon        https://abs.twimg.com/favicons/favicon.ico
-// @grant       none
+// @grant       GM_addStyle
 // @run-at      document-start
 // @inject-into content
 // ==/UserScript==
 
-function mutationObserver(args) {
-    new window.MutationObserver(function() {
-        var node;
-        if (args.tag) {
-            node = document.getElementsByTagName(args.tag)[0];
-        }
-        if (args.selector) {
-            node = document.querySelector(args.selector);
-        }
-        if (node) {
-            this.disconnect();
-            args.done(node);
-        }
-    }).observe(document, {
-        'childList': true,
-        'subtree': true
-    });
-}
+GM_addStyle(`
+  /* Hide Moments button */
+  li[class*="moments js-moments-tab"] { display: none !important; }
 
-mutationObserver({
-    tag: 'head',
-    done: function(element) {
-        var css = /* Cascading Style Sheets */
-        /* Hide Moments button */
-        'li[class*="moments js-moments-tab"] { display: none !important; }'+
+  /* Hide Who to follow */
+  div[class*="wtf-module js-wtf-module"], div[class*="WhoToFollow"],
+  li[data-component-context*="suggest_who_to_follow"] {
+    display: none !important;
+  }
 
-        /* Hide Who to follow */
-        'div[class*="wtf-module js-wtf-module"], div[class*="WhoToFollow"], '+
-        'li[data-component-context*="suggest_who_to_follow"] { display: none !important; }'+
+  /* Hide Advertise with Twitter */
+  div[class*="flex-module Footer-adsModule"] {
+    display: none !important;
+  }
 
-        /* Hide Advertise with Twitter */
-        'div[class*="flex-module Footer-adsModule"] { display: none !important; }'+
+  /* Hide While you were away... */
+  li[class*="js-stream-item"][class*="has-recap"] {
+    display: none !important;
+  }
 
-        /* Hide While you were away... */
-        'li[class*="js-stream-item"][class*="has-recap"] { display: none !important; }'+
-
-        /* Hide Live video */
-        'div[class*="VideoGuide"][class*="roaming-module"] { display: none !important; }'+
-        'div[class*="LiveVideoHomePageModuleContainer"][class*="roaming-module"] { display: none !important; }';
-
-        /* Insert the style into the page */
-        var style = document.createElement('style');
-        style.type = 'text/css';
-        style.innerHTML = css;
-        element.appendChild(style);
-    }
-});
+  /* Hide Live video */
+  div[class*="VideoGuide"][class*="roaming-module"],
+  div[class*="LiveVideoHomePageModuleContainer"][class*="roaming-module"] {
+    display: none !important;
+  }
+`);
--- youtube-disable-up-next.user.js
+++ youtube-disable-up-next.user.js
@@ -5,7 +5,7 @@
 // @description Disable "Up Next" otherwise known as AutoPlay
 // @include     http://www.youtube.com*
 // @include     https://www.youtube.com*
-// @version	    1.1.7
+// @version     1.1.7
 // @updateURL   https://github.com/konomikitten/userscripts/raw/master/youtube-disable-up-next.user.js
 // @downloadURL https://github.com/konomikitten/userscripts/raw/master/youtube-disable-up-next.user.js
 // @homepageURL https://github.com/konomikitten/userscripts

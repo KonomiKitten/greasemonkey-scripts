39a8b58c4af7ae7ca77cb4053fc513ae2c0a19d9
KonomiKitten <konomikitten@gmail.com>
2019-05-19 19:03:01 +1000

    Go back to MutationObserver as GM_addStyle seems to have race condition issues

--- reddit-tweaks.user.js
+++ reddit-tweaks.user.js
@@ -5,40 +5,69 @@
 // @description Various tweaks for Reddit
 // @include     http://www.reddit.com*
 // @include     https://www.reddit.com*
-// @version     1.3.0
+// @version     1.3.1
 // @updateURL   https://github.com/konomikitten/userscripts/raw/master/reddit-tweaks.user.js
 // @downloadURL https://github.com/konomikitten/userscripts/raw/master/reddit-tweaks.user.js
 // @homepageURL https://github.com/konomikitten/userscripts
 // @supportURL  https://github.com/konomikitten/userscripts/issues
 // @icon        https://www.redditstatic.com/desktop2x/img/favicon/favicon-96x96.png
-// @grant       GM_addStyle
+// @grant       none
 // @run-at      document-start
 // ==/UserScript==
 
-GM_addStyle(`
-  /* Hide visit new reddit */
-  button.redesign-beta-optin {
-    display: none !important;
-  }
+function mutationObserver(args) {
+  new window.MutationObserver(function() {
+    var node;
+    if (args.tag) {
+      node = document.getElementsByTagName(args.tag)[0];
+    }
+    if (args.selector) {
+      node = document.querySelector(args.selector);
+    }
+    if (node) {
+      this.disconnect();
+      args.done(node);
+    }
+  }).observe(document, {
+    'childList': true,
+    'subtree': true
+  });
+}
 
-  /* Remove color on link flairs */
-  span.linkflairlabel {
-    padding: 0 2px !important;
-    color: #555 !important;
-    background-color: #f5f5f5 !important;
-    border: 1px solid #ddd !important;
-    font-size: 10px !important;
-    line-height: 13px !important;
-    height: 13px !important;
-  }
+mutationObserver({
+  tag: 'head',
+  done: function(element) {
+  var css = `
+    /* Hide visit new reddit */
+    button.redesign-beta-optin {
+      display: none !important;
+    }
 
-  /* Remove Reddit premium banner */
-  form.premium-banner {
-    display: none !important;
-  }
+    /* Remove color on link flairs */
+    span.linkflairlabel {
+      padding: 0 2px !important;
+      color: #555 !important;
+      background-color: #f5f5f5 !important;
+      border: 1px solid #ddd !important;
+      font-size: 10px !important;
+      line-height: 13px !important;
+      height: 13px !important;
+    }
 
-  /* Remove Redesign advertisements */
-  section.redesignbetabar-js {
-    display: none !important;
+    /* Remove Reddit premium banner */
+    form.premium-banner {
+      display: none !important;
+    }
+
+    /* Remove Redesign advertisements */
+    section.redesignbetabar-js {
+      display: none !important;
+    }`;
+
+    /* Insert the style into the page */
+    var style = document.createElement('style');
+    style.type = 'text/css';
+    style.innerHTML = css;
+    element.appendChild(style);
   }
-`);
+});
--- youtube-tweaks.user.js
+++ youtube-tweaks.user.js
@@ -5,19 +5,48 @@
 // @description YouTube Tweaks
 // @include     http://www.youtube.com*
 // @include     https://www.youtube.com*
-// @version     1.0.5
+// @version     1.0.6
 // @updateURL   https://github.com/konomikitten/userscripts/raw/master/youtube-tweaks.user.js
 // @downloadURL https://github.com/konomikitten/userscripts/raw/master/youtube-tweaks.user.js
 // @homepageURL https://github.com/konomikitten/userscripts
 // @supportURL  https://github.com/konomikitten/userscripts/issues
 // @icon        https://s.ytimg.com/yts/img/favicon_32-vflOogEID.png
-// @grant       GM_addStyle
+// @grant       none
 // @run-at      document-start
 // ==/UserScript==
 
-GM_addStyle(`
-  /* Hide payed video advertisements */
-  div#offer-module {
-    display: none !important;
+function mutationObserver(args) {
+  new window.MutationObserver(function() {
+    var node;
+    if (args.tag) {
+      node = document.getElementsByTagName(args.tag)[0];
+    }
+    if (args.selector) {
+      node = document.querySelector(args.selector);
+    }
+    if (node) {
+      this.disconnect();
+      args.done(node);
+    }
+  }).observe(document, {
+    'childList': true,
+    'subtree': true
+  });
+}
+
+mutationObserver({
+  tag: 'head',
+  done: function(element) {
+    var css = `
+    /* Hide payed video advertisements */
+    div#offer-module {
+      display: none !important;
+    }`;
+
+    /* Insert the style into the page */
+    var style = document.createElement('style');
+    style.type = 'text/css';
+    style.innerHTML = css;
+    element.appendChild(style);
   }
-`);
+});
